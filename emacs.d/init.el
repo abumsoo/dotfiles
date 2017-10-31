87dde2a19e032b2d85186ebd75fcb4bd6d336c7f
Bum Kim <bumcrystlbum@gmail.com>
2017-10-31 11:03:13 -0600

    custom variables

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -74,7 +74,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/Dropbox/org-todo/tasks.org")))
+ '(org-agenda-files (quote ("~/Dropbox/org/tasks.org")))
  '(package-selected-packages (quote (org-bullets uimage key-chord evil solarized-theme)))
  '(vc-follow-symlinks t))
 (custom-set-faces
