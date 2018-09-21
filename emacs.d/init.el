61135eaec3126cbf34e9d02d1152b6c34385e1ed
Bum Kim <bumcrystlbum@gmail.com>
2018-09-21 00:45:07 -0600

    cleaned up directory structure and updated some files

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -20,6 +20,11 @@
 (tool-bar-mode -1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Ivy
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(ivy-mode 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; org
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; The following lines are always needed
@@ -34,6 +39,9 @@
 	("WAITING" . "orange")))
 ;; org-capture location
 (setq org-default-notes-file "~/Dropbox/org/tasks.org")
+;; org-capture refile targets
+(setq org-refile-targets '(("~/Dropbox/org/tasks.org")
+			   ("~/Dropbox/org/someday.org")))
 ;; org-capture templates
 (setq org-capture-templates
       '(("t" "Todo" entry (file+headline "~/Dropbox/org/tasks.org" "Tasks")
@@ -41,14 +49,14 @@
 	("n" "Notes" entry (file "~/Dropbox/org/notes.org")
 	 "* %?\n %i\n %T")))
 ;; org-agenda files
-(setq org-agenda-files (quote ("~/Dropbox/org/tasks.org"
-			       "~/Dropbox/org/projects.org")))
+(setq org-agenda-files '("~/Dropbox/org/tasks.org"
+			 "~/Dropbox/org/projects.org"))
 ;; org-bullets cool bullet points!
 (require 'org-bullets)
 (add-hook'org-mode-hook (lambda () (org-bullets-mode 1)))
 
 (org-agenda nil "a")
-(find-file "~/Dropbox/org/tasks.org")
+;; (find-file "~/Dropbox/org/tasks.org")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; evil
@@ -66,11 +74,24 @@
 (key-chord-define evil-replace-state-map "jk" 'evil-normal-state)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Other
+;; Server
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(server-start)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Theme
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(require 'doom-themes)
 ;; Added themes directory to custom load path
 ;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-;; (load-theme 'solarized-dark t)
+(load-theme 'doom-one t)
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Font
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;(set-default-font “DejaVu Sans Mono-9”)
+;;(set-fontset-font "fontset-default" "Font Awesome 5 Free:bold")
 
 ;;(set-frame-parameter (selected-frame) 'alpha '(<active> . <inactive>))
 ;;(set-frame-parameter (selected-frame) 'alpha <both>)
@@ -81,16 +102,53 @@
 (setq-default fill-column 80)
 
 ;; no backup files
-(setq make-backup-files nil)
+;; (setq make-backup-files nil)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(custom-enabled-themes (quote (doom-one)))
+ '(custom-safe-themes
+   (quote
+    ("9d9fda57c476672acd8c6efeb9dc801abea906634575ad2c7688d055878e69d6" "190a9882bef28d7e944aa610aa68fe1ee34ecea6127239178c7ac848754992df" default)))
+ '(fci-rule-color "#383838")
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(org-agenda-files (quote ("~/Dropbox/org/tasks.org")))
  '(org-todo-keywords (quote ((sequence "TODO(t)" "DONE(d)"))))
- '(package-selected-packages (quote (org-bullets uimage key-chord evil solarized-theme)))
+ '(package-selected-packages
+   (quote
+    (zenburn-theme flymake-solidity solidity-mode projectile sbt-mode ein markdown-preview-mode markdown-mode scala-mode js2-mode ivy org-bullets uimage key-chord evil solarized-theme)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3")
  '(vc-follow-symlinks t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
